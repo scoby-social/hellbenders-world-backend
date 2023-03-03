23e930f47a978d22cdeb4452654495e7f5b7669b
EliasR07 <eliasalejo01@gmail.com>
2023-03-03 15:25:34 -0400

    ft: Querying metadata from Shadow Drive to include it in the user's data, adding missing validations and removing files from SHDW DRIVE when user is deceased

---
+++ src/user/repositories/getUserByID.ts
@@ -0,0 +1,15 @@
+import { ObjectId } from "mongodb";
+import { db } from "../../config/mongoClient";
+import { User } from "../entities/user.entity";
+
+export async function getUserByID(id: string): Promise<User> {
+  const _id = new ObjectId(id);
+
+  const users = db.collection<User>("users");
+
+  const user = await users.findOne({ _id });
+
+  if (!user) return {} as User;
+
+  return user;
+}
--- src/user/services/markUserAsDeceased.ts
+++ src/user/services/markUserAsDeceased.ts
@@ -1,5 +1,19 @@
+import { deleteShadowDriveFile } from "../../shdw_drive/deleteFile";
+import { getUserByID } from "../repositories/getUserByID";
 import { deceaseUser } from "../repositories/markUserAsDeceased";
 
 export async function markUserAsDeceased(id: string): Promise<void> {
   await deceaseUser(id);
+
+  const storeAccount = process.env.SHDW_STORE_ACCOUNT!;
+
+  const user = await getUserByID(id);
+
+  const metadataShdwUrl = `https://shdw-drive.genesysgo.net/${storeAccount}/${user.username}.json`;
+  const imageShdwUrl = `https://shdw-drive.genesysgo.net/${storeAccount}/${user.username}.png`;
+
+  await Promise.all([
+    deleteShadowDriveFile(metadataShdwUrl),
+    deleteShadowDriveFile(imageShdwUrl),
+  ]);
 }
